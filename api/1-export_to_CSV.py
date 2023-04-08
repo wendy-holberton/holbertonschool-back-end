d6134e1ed65f988b6e58945e8e6d6447085fae8d
Wendy <you@example.com>
2023-04-08 06:00:43 -0700

    export to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Export to CSV"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # field names
+    fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+
+    employee_id = sys.argv[1]
+
+    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(api_url)
+    username = response.json()['username']
+
+    # data of csv file
+    api_url1 = (f"https://jsonplaceholder.typicode.com"
+                f"/todos?userId={employee_id}")
+    new_response = requests.get(api_url1)
+    data = new_response.json()
+    # name of csv file
+    filename = "USER_ID.csv"
+
+    # write to csv file
+    with open(filename, 'w', newline='') as file:
+        # create a csv writer object
+        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for r in data:
+            row = [r['userId'], username, r['completed'], r['title']]
+            csv_writer.writerow(row)
